36aeb099a1ec54c5c5b35ae3b418f41b8d298ac7
Austin Tricquet <austintricquet@gmail.com>
2020-09-12 14:12:58 -0400

    Updates

---
+++ precache-manifest.6222fd6d4e503f00fabd1141a6638158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "173293e819e0aaa89d30225a3fb29590",
+    "url": "/coin-exchange/index.html"
+  },
+  {
+    "revision": "fe729d8d072ebe780b23",
+    "url": "/coin-exchange/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2637e064c9f2564795fa",
+    "url": "/coin-exchange/static/js/2.efd261ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coin-exchange/static/js/2.efd261ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe729d8d072ebe780b23",
+    "url": "/coin-exchange/static/js/main.014698ae.chunk.js"
+  },
+  {
+    "revision": "c9fe44687b1447b49608",
+    "url": "/coin-exchange/static/js/runtime-main.ea956e0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coin-exchange/static/media/logo.5d5d9eef.svg"
+  }
+]);
